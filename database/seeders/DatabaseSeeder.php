cf3778acd68b53a8f44c96e4cbe5606df068abe6
Giuseppe Toto <billyball85@gmail.com>
2023-04-09 23:45:33 +0200

    creato seed per le candidature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Application;
+use App\Models\Status;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create([
+
+        User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@bitboss.it',
             'is_admin'=>true,
@@ -20,5 +24,18 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => \Str::random(10),
         ]);
+
+        User::factory(20)
+            ->has(Application::factory()->count(1)->state(new Sequence(
+                ['status' => Status::Accettato],
+                ['status' => Status::Scartato]
+            )))
+            ->create();
+
+        User::factory(5)
+            ->has(Application::factory()->count(1)->state(new Sequence(
+                ['status' => Status::Pendente]
+            )))
+            ->create();
     }
 }
